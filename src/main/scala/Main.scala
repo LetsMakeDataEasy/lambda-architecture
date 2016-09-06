c1127a9b91616e56fbf1623fe8328007c235cdd8
Chaoyang Jia <cyjia@thoughtworks.com>
2016-09-06 17:50:27 +0800

    add consumer

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 package com.thoughtworks.la
 
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.clients.consumer.{KafkaConsumer, ConsumerRecord}
 import scala.collection.JavaConversions._
 
 object Main {
@@ -18,6 +19,27 @@ object Main {
     val producer = new KafkaProducer[String, String](props)
     producer.send(new ProducerRecord[String, String]("my-topic", key, value))
     producer.close()
+  }
+
+  def consumeMessage() {
+     val props = new java.util.HashMap[String, Object]()
+     props.put("bootstrap.servers", "localhost:9092")
+     props.put("group.id", "test")
+     props.put("enable.auto.commit", "true")
+     props.put("auto.commit.interval.ms", "1000")
+     props.put("session.timeout.ms", "30000")
+     props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+     props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+     val consumer = new KafkaConsumer[String, String](props)
+     consumer.subscribe(List("foo", "bar"))
+     while (true) {
+       val records = consumer.poll(100)
+       for {
+         record <- records
+       } {
+         println("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value())
+       }
+     }
   }
 
   def main(args: Array[String]): Unit = {
